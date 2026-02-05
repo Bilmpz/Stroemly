120d511db5acd7f1017ea4c0809ba929a72b2fa8
Oliver Løbel <oliver.lobel@yahoo.dk>
2026-02-05 20:09:17 +0100

    Update logo on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,31 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
-      {/*Start Tekst*/}
+      
+      {/* Start Logo */}
       <header className="pt-9">
-        <div className="text-brand text-[44px] font-[480] tracking-tight">
-          Strømly<span className="align-super text-[12px] ml-1">®</span>
-        </div>
+        <img
+          src="/logo.svg"
+          alt="Strømly logo"
+          className="h-12 w-auto"
+        />
       </header>
 
-      {/*Center mini boks*/}
+      {/* Center mini boks */}
       <section className="flex-1 flex flex-col items-center justify-center text-center">
-        <div className="inline-flex items-center rounded-full border-[0.1px] border-brand-20 px-4 py-0.5">
-          <span className="text-brand text-[14px] font-[100]">
+        <div className="inline-flex items-center rounded-full border-[0.5px] border-brand-20 px-4 py-[2px] mb-10">
+          <span className="text-brand text-[14px] font-[350]">
             Lancering 2026
           </span>
         </div>
 
-        {/*Main tekst i midten*/}
-        <h1 className="mt- 0 text-brand text-[50px] leading-[120%] font-[350] tracking-tight">
+        {/* Main tekst i midten */}
+        <h1 className="text-brand text-[50px] leading-[120%] font-[350] tracking-tight">
           Vi er i gang med at lade op
         </h1>
       </section>
 
-      {/*Slut tekst*/}
+      {/* Slut tekst */}
       <footer className="pb-8 text-center">
         <p className="text-brand text-[16px] leading-[135%] font-[350]">
           Danmarks nye ladeløsning til dig på farten
